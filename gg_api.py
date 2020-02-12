e1ff1975709f2eb6b96a1cd021a86c76d189afd8
EmilyNee <emilynee2020@u.northwestern.edu>
2020-02-11 20:09:42 -0600

    now have winner stats for 2015

--- gg_api.py
+++ gg_api.py
@@ -1,7 +1,8 @@
 '''Version 0.35'''
 
 #from time import perf_counter
-
+from string import capwords
+from time import perf_counter
 from src import awards, hosts, nominees, presenters, winners
 from util.load import load_json, dump_json, read_data_from_file
 import os
@@ -226,11 +227,9 @@ def main():
         util.config.current_year = year
         data = load_json('data/', year)
         winn_map = winners.extract(data)
-        print(winn_map)
         #pres_map = presenters.extract(data)
         #print pres_map
 
-    '''
     for year in years:
         ts = perf_counter()
         data = load_json('data/', year)
@@ -252,13 +251,13 @@ def main():
         tf = perf_counter()
 
         # DISPLAY TIMES
-        print("Host names extracted in : ", round(t1 - t0, 2), " seconds.")
-        print("Award names extracted in : ", round(t2 - t1, 2), " seconds.")
-        print("Nominees extracted in : ", round(t3 - t2, 2), " seconds.")
-        print("Presenters extracted in : ", round(t4 - t3, 2), " seconds.")
-        print("Winners extracted in : ", round(tf - t4, 2), " seconds.")
-        print("Cumulative time to perform all tasks : ", round(tf - t0, 2), " seconds.")
-        print("Cumulative time to load data and perform all tasks : ", round(tf - ts, 2), " seconds.")
+        print("Host names extracted in: ", round(t1 - t0, 2), " seconds.")
+        print("Award names extracted in: ", round(t2 - t1, 2), " seconds.")
+        print("Nominees extracted in: ", round(t3 - t2, 2), " seconds.")
+        print("Presenters extracted in: ", round(t4 - t3, 2), " seconds.")
+        print("Winners extracted in: ", round(tf - t4, 2), " seconds.")
+        print("Cumulative time to perform all tasks: ", round(tf - t0, 2), " seconds.")
+        print("Cumulative time to load data and perform all tasks: ", round(tf - ts, 2), " seconds.")
 
         award_map = {}
 
@@ -276,12 +275,10 @@ def main():
                     noms = nomi_map[award]
                 except KeyError:
                     pass
-
                 try:
                     pres = pres_map[award]
                 except KeyError:
                     pass
-
                 try:
                     winn = winn_map[award]
                 except KeyError:
@@ -292,12 +289,31 @@ def main():
                 award_map[award]['winner'] = winn
 
         to_dump['award_data'] = award_map
-
-        # for testing
         dump_json('results/', year, to_dump)
 
-    print("Done.")
-    '''
+        # # HUMAN-READABLE FORMAT
+        # # HOSTS
+        # print("Host(s): ", end='')
+        # for h in to_dump['hosts']:
+        #     print(capwords(h), end=', ')
+        # print()
+        # print()
+        #
+        # # AWARDS (?)
+        #
+        # # PRESENTERS, NOMINEES, and WINNERS
+        # for ad, map in to_dump['award_data'].items():
+        #     print("Award: ", capwords(ad))
+        #     print("Nominee(s): ", end='')
+        #     for n in map['nominees']:
+        #         print(capwords(n), end=', ')
+        #     print()
+        #     print("Presenter(s): ", end='')
+        #     for p in map['presenters']:
+        #         print(capwords(p), end=', ')
+        #     print()
+        #     print("Winner: ", capwords(map['winner']))
+        #     print()
 
     return
 
